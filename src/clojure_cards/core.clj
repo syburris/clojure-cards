be8c86d3313ac3ecbaf5f9349b2c62e38b75de72
Steven Burris <stevenburris.jr@gmail.com>
2016-11-03 11:55:54 -0400

    MATH....

--- src/clojure_cards/core.clj
+++ src/clojure_cards/core.clj
@@ -1,6 +1,38 @@
 (ns clojure-cards.core
   (:gen-class))
 
+
+(def suits [:clubs :spades :hearts :diamonds])
+(def ranks (range 1 14))
+
+(defn create-deck []
+  (set
+    (for [suit suits
+          rank ranks]
+        {:suit suit :rank rank})))
+
+(defn create-hands [deck]
+  (set
+    (for [c1 deck
+          c2 (disj deck c1)
+          c3 (disj deck c1 c2)
+          c4 (disj deck c1 c2 c3)]
+      #{c1 c2 c3 c4})))
+
+(def test-hand #{{:suit :spades :rank 1}
+                 {:suit :spades :rank 2}
+                 {:suit :spades :rank 3}
+                 {:suit :spades :rank 4}})
+
+
+
+(defn flush? [hand]
+  (= 1 (count (set (map :suit hand)))))
+  
+
 (defn -main [& args]
-  (println "Hello, world!"))
+  (let [deck (create-deck)
+        hands (create-hands deck)
+        hands (filter flush? hands)]
+    (println (count hands))))
 
